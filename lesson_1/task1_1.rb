f1baea31dfa155835d583768758f048c33900e4d
freakeno16 <122446249+freakeno16@users.noreply.github.com>
2023-02-09 16:28:40 +0500

    Update task1_1.rb
    поправил отступы

--- lesson_1/task1_1.rb
+++ lesson_1/task1_1.rb
@@ -3,9 +3,9 @@ name = gets.chomp
 name.capitalize!
 puts 'Height: '
 height = gets.to_i
-optimal = (height - 110)*1.15
-        if optimal < 0
-            puts "Your weight is optimal!"
-        else
-            puts "#{name}, your optimal height is #{optimal}"
-        end
+optimal = (height - 110) * 1.15
+  if optimal < 0
+    puts "Your weight is optimal!"
+  else
+    puts "#{name}, your optimal height is #{optimal}"
+  end
