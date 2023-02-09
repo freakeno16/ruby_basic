582735cfbb4978d4a198730c74673751793ecc8b
freakeno16 <122446249+freakeno16@users.noreply.github.com>
2023-02-09 16:46:34 +0500

    Update task1_3.rb
    теперь код смотрибелен)

--- lesson_1/task1_3.rb
+++ lesson_1/task1_3.rb
@@ -1,29 +1,35 @@
-puts 'vvedite 1 storonu: '
+puts 'enter the first side of triangle: '
 a = gets.to_i
-puts 'vvedite 2 storonu: '
+puts 'enter the second one: '
 b = gets.to_i
-puts 'vvedite 3 storonu: '
+puts 'enter the third one: '
 c = gets.to_i
-    if a == b && a == c
-        puts 'ravnostor'
-    end
-    if a == b && a != c || a == c && a != b || b == c && b != a
-        puts 'ravnobedr'
-    end
-    if a**2 == b**2 + c**2 
-        gip = a**2
-    elsif
-        b**2 == a**2 + c**2
-        gip = b**2
-    elsif
-        c**2 == a**2 + b**2
-        gip = c**2
-    end
-    if 
-        gip == b**2 + c**2
-        puts 'pryamoug'
-    elsif gip == a**2 + c**2
-        puts 'pryamoug'
-    elsif gip == a**2 + b**2
-        puts 'pryamoug'
-    end
+
+  if a == b && a == c
+    puts 'triangle is equilateral'
+  end
+  
+  if a == b && a != c || a == c && a != b || b == c && b != a
+    puts 'triangle is isosceles'
+  end
+  
+  if a ** 2 == b ** 2 + c ** 2 
+    gip = a ** 2
+  elsif
+    b ** 2 == a ** 2 + c ** 2
+    gip = b ** 2
+  elsif
+    c ** 2 == a ** 2 + b ** 2
+    gip = c ** 2
+  end
+  
+  if
+    gip == b ** 2 + c ** 2
+    puts 'triangle is rectangular'
+  elsif 
+    gip == a ** 2 + c ** 2
+    puts 'triangle is rectangular'
+  elsif 
+    gip == a ** 2 + b ** 2
+    puts 'triangle is rectangular'
+  end
