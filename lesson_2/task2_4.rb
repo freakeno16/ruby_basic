e37c0d29b3d49d70c20a22e7960f33d44124702d
freakeno16 <122446249+freakeno16@users.noreply.github.com>
2023-02-14 23:53:57 +0500

    Update task2_4.rb
    убрал транслит, гугл транслейтер в помощь. vowel - гласная буква, оказывается

--- lesson_2/task2_4.rb
+++ lesson_2/task2_4.rb
@@ -2,5 +2,5 @@ abc_array = ["a", "e", "i", "o", "u", "y"]
 
 abc_hash = {}
 
-("a".."z").each_with_index { |bukva, i| abc_hash[bukva] = i if abc_array.include?(bukva) }
+("a".."z").each_with_index { |vowel, i| abc_hash[vowel] = i if abc_array.include?(vowel) }
 p abc_hash
