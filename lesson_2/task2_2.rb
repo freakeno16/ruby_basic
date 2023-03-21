052c92a07d769b3f299ddbe0f6a42311b6b87af7
freakeno16 <nikitaotkidach@gmail.com>
2023-03-21 15:19:03 +0500

    idi nahuy

--- lesson_2/task2_2.rb
+++
@@ -1,7 +0,0 @@
-array = []
-for i in 10..100 do
-  if i % 5 == 0
-  array << i ##хуйавафв
-  p i
-  end
-end
--- lesson_2/task2_3.rb
+++
@@ -1,8 +0,0 @@
-abc = {}
-abc[:a] = 1
-abc[:e] = 2
-abc[:i] = 3
-abc[:o] = 4
-abc[:u] = 5
-abc[:y] = 6
-p abc
