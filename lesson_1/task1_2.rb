b4cdd90f61e13f95b954833ffbabc8553e42c1b1
freakeno16 <nikitaotkidach@gmail.com>
2023-04-04 18:44:18 +0500

    suka

---
+++ lesson_1/task1_2.rb
@@ -0,0 +1,7 @@
+puts 'enter a:'
+a = gets.to_i
+puts 'enter h: '
+h = gets.to_i
+s = 0.5 * a * h
+puts "s = #{s}"
+    
---
+++ lesson_2/task2_6.rb
@@ -0,0 +1,27 @@
+hash = {}
+final_cost = 0
+
+loop do 
+  p "name: "
+  name = gets.chomp
+  
+  p "Cost: "
+  cost = gets.to_f
+  
+  p "How many: "
+  count = gets.to_f
+
+  each_cost = cost * count
+  
+  hash[name.to_sym] = { cost: cost, count: count, each_cost: each_cost }
+  
+  final_cost += each_cost
+  
+  p 'print stop if u want to stop:'
+
+  break if gets.chomp == "stop"
+end
+
+p hash
+
+p "final cost: #{final_cost}"
