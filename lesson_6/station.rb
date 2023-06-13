c91e5054935e8173138b7b38af8536e2ea8ecd38
freakeno16 <nikitaotkidach@gmail.com>
2023-06-13 21:22:33 +0500

    aboba

---
+++ lesson_6/station.rb
@@ -0,0 +1,34 @@
+class Station 
+  @stations = {}
+  
+  # include InstanceCounter
+
+  attr_reader :trains, :name
+
+  class << self
+    def all_routes
+      @routes
+    end
+  
+    def add_station(station)
+      @stations[station.name] = station
+    end
+  end
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+  
+  def add_train(train)
+    @trains << train
+  end
+
+  def send_train(train)
+    @trains.delete(train)
+  end
+
+  def trains_by_type(type)
+    @trains.select { |t| t.type == type }
+  end
+end
---
+++ lesson_6/wagon.rb
@@ -0,0 +1,24 @@
+require_relative 'company_name.rb'
+
+class Wagon
+  @wagons = {}
+
+  include CompanyName
+
+  attr_reader :name, :type
+
+  class << self 
+    def all_routes
+      @routes
+    end
+  
+    def add_route(route)
+      @routes[route.name] = route
+    end
+  end
+  
+  def initialize(name, type)
+    @name = name
+    @type = type
+  end
+end
