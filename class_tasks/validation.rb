1ea503f1faf655a6db5b9c2b45a5266a78320d52
freakeno16 <nikitaotkidach@gmail.com>
2023-11-27 22:14:28 +0500

    in work..

--- class_tasks/validation.rb
+++ class_tasks/validation.rb
@@ -1,30 +1,30 @@
 module Validation
-
   def validate(attr_name, val_type, *args)
-    var_name = "@#{attr_name}".to_sym
+    args.each do |arg|
+      var_name = "@#{attr_name}".to_sym
 
-    define_method(attr_name) { instance_variable_get(var_name) } 
-    define_method("#{attr_name}=") do |value|
-      if val_type == :presence
-        yield if block_given?
-        raise "huy tebe eblan" if value == '' || value == nil
-        instance_variable_set(var_name, value)
-      elsif val_type == :format
-        yield if block_given?
-        raise "huy tebe loshara" if value !~ /A-Z{0,3}/
-        instance_variable_set(var_name, value)
-      elsif val_type == :type
-        yield if block_given?
-        raise "huy tebe bomjara" if value != self.class
-        instance_variable_set(var_name, value)
+      define_method(attr_name) { instance_variable_get(var_name) } 
+
+      define_method("#{attr_name}=") do |value|
+        case val_type
+        when :presence
+          raise "Value can't be empty string or nil!" if value == '' || value == nil
+          instance_variable_set(var_name, value)
+        when :format
+          raise "Value must be match the regexp!" if value !~ arg #/A-Z{0,3}/
+          instance_variable_set(var_name, value)
+        when :type
+          raise "Value must be match the class!" if value != arg #self.class
+          instance_variable_set(var_name, value)
+        end
       end
     end
   end
 end
- 
+
 class Foo
   
   extend Validation
   
-  validate :name, :type, Foo
+  validate :name, :presence
 end
