9b08d4d8402cff1773c39004d5764d60ea6f00ce
freakeno16 <nikitaotkidach@gmail.com>
2023-11-30 21:57:34 +0500

    Nihuya ne ponyatno koroche

--- class_tasks/validation.rb
+++ class_tasks/validation.rb
@@ -1,35 +1,43 @@
 module Validation
-  def validate(name, type, *args)
-    var_name = "@#{name}".to_sym
+  def self.included(base)
+    base.extend ClassMethods
+    base.include InstanceMethods
+  end
 
-    define_method(name) { instance_variable_get(var_name) }
-    define_method("#{name}=") do |value|
-      if type == :presence
-        raise "Value can't be empty string or nil!" if ['', nil].include?(value)
+  module ClassMethods
+    def validate(name, type, *args)
+      var_name = "@#{name}".to_sym
 
-        instance_variable_set(var_name, value)
-      end
-
-      args.each do |arg|
-        case type
-        when :format
-          raise 'Value must be match the regexp!' if value !~ arg
+      define_method(name) { instance_variable_get(var_name) }
+      define_method("#{name}=") do |value|
+        if type == :presence
+          raise "Value can't be empty string or nil!" if ['', nil].include?(value)
 
           instance_variable_set(var_name, value)
-        when :type
-          raise 'Value must be match the class!' unless value.is_a?(arg)
+        end
 
-          instance_variable_set(var_name, value)
+        args.each do |arg|
+          case type
+          when :format
+            raise 'Value must be match the regexp!' if value !~ arg
+
+            instance_variable_set(var_name, value)
+          when :type
+            raise 'Value must be match the class!' unless value.is_a?(arg)
+
+            instance_variable_set(var_name, value)
+          end
         end
       end
     end
   end
+
+  module InstanceMethods
+    def validate!
+    end
+  end
 end
 
-class Foo
-  extend Validation
 
-  validate :name, :presence
-  validate :number, :format, /^[a-z\d]{3}-*[a-z\d]{2}$/
-  validate :refer, :type, Foo
-end
+
+
