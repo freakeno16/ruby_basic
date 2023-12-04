4b184d3938df2d5c29d17a585492a866abed7650
freakeno16 <nikitaotkidach@gmail.com>
2023-12-04 14:16:28 +0500

    Исправил недочеты и рубокопнул

--- class_tasks/accessors.rb
+++ class_tasks/accessors.rb
@@ -1,41 +1,42 @@
-module Accessors 
-  def attr_accessor_with_history(*args)
-
-    args.each do |arg|
-      var_name = "@#{arg}".to_sym
-      define_method(arg) { instance_variable_get(var_name) }
-
-      define_method("#{arg}=") do |value|
-        
-        if instance_variable_get("@#{arg}_old")
-          instance_variable_set("@#{arg}_old", instance_variable_get("@#{arg}_old") << value)
-          instance_variable_set(var_name, value)
-        else
-          instance_variable_set(var_name, value)
-          instance_variable_set("@#{arg}_old", [value])
-        end
-      end
-        define_method("#{arg}_history") { instance_variable_get("@#{arg}_old") }
-    end
+module Accessors
+  def self.included(base)
+    base.extend ClassMethods
   end
 
-  def strong_attr_accessor(attr_name, attr_class)
-    var_name = "@#{attr_name}".to_sym
+  module ClassMethods
+    def attr_accessor_with_history(*args)
+      args.each do |arg|
+        var_name = "@#{arg}".to_sym
+        define_method(arg) { instance_variable_get(var_name) }
 
-    define_method(name) { instance_variable_get(var_name) }
-    define_method("#{attr_name}=") do |value| 
-      if value.is_a? attr_class
+        define_method("#{arg}=") do |value|
+          if instance_variable_get("@#{arg}_old")
+            instance_variable_set("@#{arg}_old", instance_variable_get("@#{arg}_old") << value)
+            instance_variable_set(var_name, value)
+          else
+            instance_variable_set(var_name, value)
+            instance_variable_set("@#{arg}_old", [value])
+          end
+        end
+        define_method("#{arg}_history") { instance_variable_get("@#{arg}_old") }
+      end
+    end
+
+    def strong_attr_accessor(attr_name, attr_class)
+      var_name = "@#{attr_name}".to_sym
+
+      define_method(attr_name) { instance_variable_get(var_name) }
+      define_method("#{attr_name}=") do |value|
+        raise "Wrong attr_class, must be #{attr_class}, retry please" unless value.is_a? attr_class
+
         instance_variable_set(var_name, value)
-      else
-        raise "Wrong attr_class, must be #{attr_class}, retry please"
       end
     end
   end
 end
 
 class Foo
-  
-  extend Accessors
+  include Accessors
 
   # attr_accessor_with_history :a, :b
   strong_attr_accessor :a, Integer
